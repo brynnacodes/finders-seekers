fe65c25ab613350b2c133beea5174d1285c1a92a
Brynna <brynna.mccarter@gmail.com>
2017-03-24 08:55:15 -0700

    add update question functionality

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,11 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuetsionForm: false,
   actions: {
     delete(question) {
       if (confirm("Are you sure you want to delete this question?")) {
         this.sendAction('destroyQuestion', question)
       }
+    },
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+    update(question) {
+      var params = {
+      author: this.get('author'),
+      title: this.get('title'),
+      content: this.get('content'),
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('update', question, params);
     }
   }
 });
